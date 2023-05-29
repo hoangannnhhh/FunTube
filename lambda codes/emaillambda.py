f4485bff7704ed0ee0871ac2ee46278d7825fa17
hoangannnhhh <hoangannnhhh@gmail.com>
2023-05-29 16:55:08 -0400

    adding registeredemaillambda code

---
+++ lambda codes/emaillambda.py
@@ -0,0 +1,71 @@
+import json
+import boto3
+def lambda_handler(event, context):
+    #showID = next(iter(event.keys()))
+    # Retrieve the email data from the event object
+    #email = event[showID]['email']
+    # Create a DynamoDB client
+    
+    dynamodb = boto3.client('dynamodb')
+ 
+    # Write the email to the DynamoDB table
+    response = dynamodb.put_item(
+        TableName='funtubeDB2',
+        Item={
+        'id': {'S': event['id']},
+        'email': {'S': event['payload']['email']},
+        'name': {'S': event['payload']['eventData']['name']},
+        'location': {'S': event['payload']['eventData']['location']},
+        'date': {'S': event['payload']['eventData']['date']},
+        'time': {'S': event['payload']['eventData']['time']},
+        'min': {'S': event['payload']['eventData']['min']},
+        'max': {'S': event['payload']['eventData']['max']}
+                }
+            )
+    # Subscribe the user's email to an SNS topic
+    sns = boto3.client('sns')
+    # Replace the sns_topic_arn with your SNS topic ARN
+    sns_topic_arn = 'arn:aws:sns:us-east-1:035082996281:email'
+    try:
+        response = sns.subscribe(
+            TopicArn=sns_topic_arn,
+            Protocol='email',
+            Endpoint=event['payload']['email']
+        )
+        # Customize the notification message here
+        notification_message = 'Thank you for subscribing to Funtube, we will keep you updated on you selected events price.'
+        # Send the customized notification to the user
+        response = sns.publish(
+            TopicArn=sns_topic_arn,
+            Message=notification_message
+        )
+
+        # Return a response
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Email registered successfully and user subscribed to notifications')
+        }
+    except Exception as e:
+        # Handle any errors that occur during the subscription process
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Error subscribing user to notifications: {}'.format(str(e)))
+        }
+
+
+#Configure Test Event
+# {
+#   "id": "2342543",
+#   "payload": {
+#     "email": "testemail4@test.com",
+#     "eventData": {
+#       "name": "Taylor Swift",
+#       "location": "Atlanta",
+#       "venue": "here",
+#       "date": "2023-05-19",
+#       "time": "18:30:00",
+#       "min": "9",
+#       "max": "10"
+#     }
+#   }
+# }
---
+++ lambda codes/registeredemaillambda.py
@@ -0,0 +1,92 @@
+import boto3
+import requests
+import json
+
+dynamodb_table_name = "funtubeDB2"
+
+# Initialize AWS clients
+dynamodb = boto3.resource('dynamodb')
+sns = boto3.client('sns')
+
+def lambda_handler(event, context):
+    # Retrieve artist and user details from event
+    table = dynamodb.Table(dynamodb_table_name)
+    response = table.scan()
+
+    for item in response['Items']:
+        show_id = item['id']
+        email = item['email']
+
+        # Set up the Ticketmaster API endpoint URL
+        #url = f'https://app.ticketmaster.com/discovery/v2/events/G5vjZ9ISF_0vU.json?apikey=7kyWM9xf2TVWoiEpA7lEXDsAFLjvAGN1'
+        url = f'https://app.ticketmaster.com/discovery/v2/events/{show_id}.json?apikey=7kyWM9xf2TVWoiEpA7lEXDsAFLjvAGN1'
+        # Make the API request
+        response = requests.get(url)
+        # Check for a successful response
+        if response.status_code == 200:
+            # Parse the JSON response
+            data = json.loads(response.text)
+            # Extract the desired information from the response
+            event_data = {
+                'name' : data['name'],
+                'location' : data['_embedded']['venues'][0]['city']['name'],
+                'venue' : data['_embedded']['venues'][0]['name'],
+                'date' : data['dates']['start']['localDate'],
+                'time' : data['dates']['start']['localTime'],
+                'id' : data['id'],
+                'min' : data['priceRanges']['min'],
+                'max' : data['priceRanges']['max']
+                }
+            # Update the item in DynamoDB
+            update_dynamodb(table, show_id, event_data)
+
+def update_dynamodb(table, show_id, event_data):
+    # Update the item in DynamoDB
+    table.update_item(
+        Key={
+            'id': show_id
+        },
+        UpdateExpression='SET #name = :name, #location = :location, #venue = :venue, #date = :date, #time = :time, #min = :min, #max = :max',
+        ExpressionAttributeNames={
+            '#name': 'name',
+            '#location': 'location',
+            '#venue': 'venue',
+            '#date': 'date',
+            '#time': 'time',
+            '#min': 'min',
+            '#max': 'max'
+            },
+        ExpressionAttributeValues={
+            ':name': event_data['name'],
+            ':location': event_data['location'],
+            ':venue': event_data['venue'],
+            ':date': event_data['date'],
+            ':time': event_data['time'],
+            ':min': event_data['min'],
+            ':max': event_data['max']
+            }
+    )
+ # Subscribe the user's email to an SNS topic
+    sns = boto3.client('sns')
+    sns_topic_arn = 'arn:aws:sns:us-east-1:035082996281:funtuberegisteredemail'
+    try:
+        response = sns.subscribe(
+            TopicArn=sns_topic_arn,
+            Protocol='email',
+            Subject='Ticket Price Update'
+        )
+        # Customize the notification message here
+        notification_message = f"This is the updated information for {artist_name} :\n{json.dumps(ticket_prices)}"
+        # Send the customized notification to the user
+        response = sns.publish(
+            TopicArn=sns_topic_arn,
+            Message=notification_message
+        )
+
+
+
+#Configure Test Event
+# {
+#   "id": "G5vjZ9ISF_0vU",
+#   "email": "test2@gt.com"
+# }
